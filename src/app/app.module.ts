e7bc671c93b8714f24ec2711a8fd69024dc28689
coralharmut <coral.harmut@roche.com>
2024-05-02 11:39:50 +0200

    add driver-selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { OneWebComponentsAngularModule } from '@one/angular';
 import { CardComponent } from '../components/card/card.component';
 import { AppComponent } from './app.component';
+import { DriverSelectorComponent } from '../components/driver-selector/driver-selector.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent],
+  declarations: [AppComponent, CardComponent, DriverSelectorComponent],
   imports: [BrowserModule, FormsModule, OneWebComponentsAngularModule],
   bootstrap: [AppComponent]
 })
---
+++ src/components/driver-selector/driver-selector.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-selector',
+  templateUrl: './driver-selector.component.html',
+  styleUrl: './driver-selector.component.css'
+})
+export class DriverSelectorComponent {
+
+}
