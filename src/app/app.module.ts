34716ec859d4904dd5b2e63a6c6495fb15f043fd
coralharmut <coral.harmut@roche.com>
2024-05-02 12:00:01 +0200

    add schema.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { OneWebComponentsAngularModule } from '@one/angular';
-import { CardComponent } from '../components/card/card.component';
+import { CardComponent } from './components/card/card.component';
 import { AppComponent } from './app.component';
-import { DriverSelectorComponent } from '../components/driver-selector/driver-selector.component';
+import { DriverSelectorComponent } from './components/driver-selector/driver-selector.component';
 
 @NgModule({
   declarations: [AppComponent, CardComponent, DriverSelectorComponent],
